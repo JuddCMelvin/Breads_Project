838fafaff03854c026bc636c712c9cbc354f6e6f
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-04-20 13:42:16 -0400

    got rye bread

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,8 +4,12 @@ const breads = express.Router()
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.send(Bread)
+    res.render('index', {
+        breads: Bread
+    })
+  // res.send(Bread)
 })
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])

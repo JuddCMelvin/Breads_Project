8e38980d8c66699aa879f8f7dee9c2cb3a15df6d
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-04-26 22:30:47 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -10,6 +11,8 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -19,6 +22,11 @@ app.get('/', (req, res) => {
 // BREADS
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
 
 // LISTEN
 app.listen(PORT, () => {
